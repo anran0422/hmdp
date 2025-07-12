40d53a4af107a582687ae73da4e2cd4d021599b4
An_ran <15804292449@163.com>
2025-07-12 21:19:06 +0800

    商铺查询缓存：商铺查询缓存

--- src/main/java/com/hmdp/constant/RedisConstants.java
+++ src/main/java/com/hmdp/constant/RedisConstants.java
@@ -1,13 +1,20 @@
 package com.hmdp.constant;
 
 public class RedisConstants {
+
+    /**
+     * 登录相关
+     */
     public static final String LOGIN_CODE_KEY = "login:code:";
     public static final Long LOGIN_CODE_TTL = 2L;
     public static final String LOGIN_USER_KEY = "login:token:";
     public static final Long LOGIN_USER_TTL = 30L;
 
-    public static final Long CACHE_NULL_TTL = 2L;
 
+    public static final Long CACHE_NULL_TTL = 2L;
+    /**
+     * 店铺缓存
+     */
     public static final Long CACHE_SHOP_TTL = 30L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
 
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.constant.RedisConstants;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +26,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result getShopById(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        //  1. 从 Redis 查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //  2. 是否命中缓存
+        if(StrUtil.isNotBlank(shopJson)) {
+            //  3. 命中，直接返回商铺
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        //  4. 未命中，根据 id 查询数据库
+        Shop shop = this.getById(id);
+        //  5. 不存在，返回 404
+        if(ObjectUtil.isNull(shop)) {
+            return Result.fail("查询商铺不存在!");
+        }
+        //  6. 存在，写入到 Redis 中 并且返回结果
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+
+        return Result.ok(shop);
+    }
 }
